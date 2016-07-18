f7097aa195173764dd6e0954c588262fba82d72a
Dan Ross <ross9885@gmail.com>
2016-07-18 14:51:09 +0300

    Fix db test: use test.cb and t.end for async tests

--- test/db-test.js
+++ test/db-test.js
@@ -1,31 +1,28 @@
 import mongoose from "mongoose";
 import test from "ava";
 
-(() => {
-	test("Create a Db and then drop it", t => {
-		t.plan(1);
 
-		const dbName = t.title.replace(/\W+/g, "-");
+test.cb("Create a Db and then drop it", t => {
+	const dbName = t.title.replace(/\W+/g, "-");
 
-		const db = mongoose.connect(`mongodb://localhost/test-${dbName}`, error => {
+	const db = mongoose.connect(`mongodb://localhost/test-${dbName}`, error => {
+		if (error) {
+			console.error("failed to create db", error);
+			return t.fail(error);
+		}
+
+		console.log("created db");
+
+		db.connection.db.dropDatabase(error => {
 			if (error) {
-				console.error("failed to create db", error);
+				console.error("failed to drop db", error);
 				return t.fail(error);
 			}
 
-			console.log("created db");
+			console.log("dropped db");
 
-			db.connection.db.dropDatabase(error => {
-				if (error) {
-					console.error("failed to drop db", error);
-					return t.fail(error);
-				}
-
-				console.log("dropped db");
-
-				t.true(true); // Plan to execute this, because is seems like the dropDatabase callback isn't called at all.
-				return t.pass();
-			});
+			t.pass();
+			t.end();
 		});
 	});
-})();
+});
